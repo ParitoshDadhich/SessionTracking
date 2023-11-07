64823ddd94016142d4abba6adbb2b7ef530ac202
ParitoshDadhich <pdadhich0481@gmail.com>
2023-11-07 10:56:51 +0530

    implemented cookie functionality to save user's state

--- src/main/java/com/tracking/Servlet1.java
+++ src/main/java/com/tracking/Servlet1.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -16,10 +17,13 @@ public class Servlet1 extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		PrintWriter writer = response.getWriter();
-		String userName = request.getParameter("name");
+		String userName = request.getParameter("user_name");
 		writer.print("<h1>Hello " + userName + " Welcome to page</h1>");
 		writer.println("<h2>< <a href='servlet2'> Go to Servlet2</a></h2>");
 		
+		// create a cookie
+		Cookie cookie = new Cookie("user_name", userName);
+		response.addCookie(cookie);
 	}
 
 	/**
--- src/main/java/com/tracking/Servlet2.java
+++ src/main/java/com/tracking/Servlet2.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -16,9 +17,29 @@ public class Servlet2 extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		PrintWriter writer = response.getWriter();
-		String userName = request.getParameter("name");
-		writer.print("<h1>Hello " + userName + " Welcome to page</h1>");
-		writer.println("<h2>Thank you!</h2>");
+		String userName = null;
+		
+		// getting all the cookies
+		Cookie cookies[] = request.getCookies();
+		boolean flag = false;
+		if(cookies == null) {
+			writer.println("<h1>You are a new user, Go to home page and submit your name!</h1>");
+		} else {
+			for(Cookie cookie: cookies) {
+				String cookieName = cookie.getName();
+				if(cookieName.equals("user_name")) {
+					flag = true;
+					userName = cookie.getValue();
+				}
+			}
+		}
+		
+		if(flag) {
+			writer.print("<h1>Hello " + userName + " Welcome to page</h1>");
+			writer.println("<h2>Thank you!</h2>");
+		} else {
+			writer.println("<h1>You are a new user, Go to home page and submit your name!</h1>");
+		}
 	}
 
 	/**
